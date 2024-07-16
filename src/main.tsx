8198890c00271ac01cabb6a915b59cedaa4af1a3
Darwin Prayoga <darwinprayoga@Darwins-MacBook-Air.local>
2024-07-16 14:24:53 +0700

    fixing the font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
 import "@fontsource/inter";
+import "@fontsource/inter/400.css"; // Normal 400
+import "@fontsource/inter/400-italic.css"; // Italic 400
+import "@fontsource/inter/700.css"; // Normal 700
+import "@fontsource/inter/700-italic.css"; // Italic 700
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

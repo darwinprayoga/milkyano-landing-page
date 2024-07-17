a492920d5358f15ddf18bf50a8342732cc7c6709
Darwin Prayoga <darwinprayoga@Darwins-MacBook-Air.local>
2024-07-17 13:49:46 +0700

    responsive part#1

---
+++ src/components/Pointer.tsx
@@ -0,0 +1,15 @@
+export default function Pointer(props: { classname: string }) {
+  return (
+    <svg
+      className={props.classname}
+      viewBox="0 0 54 53"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M35.4 53C32.4 44.2 29.4 35.4 26.4 26.6C17.6 23.6 8.8 20.6 0 17.6C17.7 11.7 35.4 5.8 53.1 0C47.2 17.7 41.3 35.3 35.4 53Z"
+        className="fill-current"
+      />
+    </svg>
+  );
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,3 @@
-import pointer from "../assets/pointer.png";
-import pointer2 from "../assets/pointer2.png";
 import gradient from "../assets/Gradient.png";
 import astronout3d from "../assets/Astronout3d.png";
 import threeAstronout3d from "../assets/ThreeAstronout3d.png";
@@ -12,6 +10,7 @@ import footer from "../assets/Footer.png";
 import MilkyanoOverlay from "../components/MilkyanoOverlay";
 import MetaBanner from "../components/MetaBanner";
 import MilkyanoCrown from "../components/MilkyanoCrown";
+import Pointer from "../components/Pointer";
 
 function LandingPage() {
   return (
@@ -20,11 +19,11 @@ function LandingPage() {
         <img
           width={1000}
           height={1000}
-          className="absolute -left-56 -top-52"
+          className="absolute md:-left-56 md:-top-52 -top-20 -right-20 md:right-0"
           src={gradient}
           alt="blue gradient"
         />
-        <div className="container place-self-center grid grid-cols-3 m-4 mb-40">
+        <div className="container place-self-center grid grid-cols-3 m-4 mb-40 px-4">
           <div />
           <img
             width={80}
@@ -35,11 +34,11 @@ function LandingPage() {
           />
           <button className="buttonOn place-self-end">
             BOOK CALL
-            <img width={15} height={15} src={pointer} alt="pointer" />
+            <Pointer classname="buttonIcon" />
           </button>
         </div>
 
-        <div className="flex flex-col gap-3 justify-center items-center text-center w-1/2 self-center mb-60">
+        <div className="flex flex-col gap-3 justify-center items-center text-center md:w-1/2 self-center mb-40 md:mb-60 px-6">
           <MilkyanoOverlay classname="w-full absolute z-0" />
           <MetaBanner classname="w-52 z-10" />
           <div className="flex flex-col md:scale-125 md:my-2 z-10">
@@ -60,16 +59,16 @@ function LandingPage() {
           </sub>
           <button className="buttonSemi z-10">
             START NOW
-            <img width={15} height={15} src={pointer} alt="pointer" />
+            <Pointer classname="buttonIcon" />
           </button>
         </div>
 
-        <div className="w-4/5 text-end place-self-center my-20 relative mb-60">
+        <div className="md:w-4/5 text-end place-self-center my-20 relative mb-40 md:mb-60 px-6 w-full">
           <img
             width={1000}
             height={1000}
             src={astronout3d}
-            className="absolute -top-32"
+            className="absolute md:-top-32 bottom-0 -left-10 -z-10"
             alt="the Astronout 3d"
           />
 
@@ -83,17 +82,17 @@ function LandingPage() {
             </div>
             <button className="buttonSemi z-10">
               FREE CONSULTATION
-              <img width={15} height={15} src={pointer} alt="pointer" />
+              <Pointer classname="buttonIcon" />
             </button>
           </div>
         </div>
 
-        <div className="w-4/5 place-self-center my-20 relative mb-60">
+        <div className="md:w-4/5 place-self-center my-20 relative flex flex-col mb-40 md:mb-60 px-6 gap-24">
           <img
             width={350}
             height={350}
             src={iphoneMockup}
-            className="absolute right-0"
+            className="md:absolute order-last right-0 -z-10"
             alt="the Iphone Mockup"
           />
 
@@ -107,7 +106,7 @@ function LandingPage() {
               <h1>YOUR CREATIVE IN</h1>
               <h1>MELBOURNE</h1>
             </div>
-            <div className="flex flex-col gap-3 w-1/2">
+            <div className="flex flex-col gap-3 md:w-1/2">
               <b>
                 As a company focused on creating engaging ads with a deep
                 understanding of Facebook’s algorithm and a talented team, we
@@ -131,19 +130,23 @@ function LandingPage() {
           </div>
         </div>
 
-        <div className="flex justify-center relative bg-gradient-to-b from-black to-[#01126B]/35">
-          <div className="w-4/5 flex flex-col items-start gap-3 z-10">
+        <div className="flex justify-center relative bg-gradient-to-b from-black to-[#01126B]/35 px-6">
+          <div className="md:w-4/5 flex flex-col items-start gap-3 z-10">
             <MilkyanoCrown classname="w-10 text-[#1374F6]" />
             <div className="flex flex-col">
               <h1>
                 YOU NEED THE <span className="textGradient">BEST</span>
               </h1>
-              <h1>
-                <span className="textGradient">TEAM</span> TO SUCCEED IN
+              <h1 className="md:hidden block">
+                <span className="textGradient">TEAM</span> TO SUCCEED IN THIS
+                WORLD
               </h1>
-              <h1>THIS WORLD</h1>
+              <h1 className="md:block hidden">
+                <span className="textGradient">TEAM</span> TO SUCCEED
+              </h1>
+              <h1 className="md:block hidden">THIS WORLD</h1>
             </div>
-            <div className="flex flex-col gap-3 w-1/2">
+            <div className="flex flex-col gap-3 md:w-1/2">
               <b>
                 Finding the winning creative is the hardest part of increasing
                 your ad budget every day on Facebook Ads. Many e-commerce brands
@@ -161,7 +164,7 @@ function LandingPage() {
             </div>
             <button className="buttonSemi z-10">
               GET US
-              <img width={15} height={15} src={pointer} alt="pointer" />
+              <Pointer classname="buttonIcon" />
             </button>
 
             <img
@@ -174,16 +177,18 @@ function LandingPage() {
           </div>
         </div>
 
-        <div className="bg-[#2797FF] flex justify-center py-32 mb-40">
-          <div className="w-4/5 flex justify-center gap-8 relative">
-            <img
-              width={350}
-              height={350}
-              src={milkyanoRobot}
-              alt="the Astronout 3d"
-            />
+        <div className="bg-[#2797FF] flex justify-center py-32 mb-40 px-6">
+          <div className="md:w-4/5 flex flex-col md:flex-row justify-center gap-8 relative">
+            <div className="transform scale-x-[-1] md:scale-x-100 w-36 md:w-60 place-self-end md:place-self-center">
+              <img
+                width={350}
+                height={350}
+                src={milkyanoRobot}
+                alt="the Astronout 3d"
+              />
+            </div>
 
-            <div className="w-1/3 flex flex-col gap-3 z-10">
+            <div className="md:w-1/3 flex flex-col gap-3 z-10">
               <MilkyanoCrown classname="w-10" />
               <h1>OUR SERVICES</h1>
               <sub>
@@ -208,13 +213,13 @@ function LandingPage() {
               </sub>
               <button className="buttonNegative w-max">
                 GET US
-                <img width={15} height={15} src={pointer2} alt="pointer" />
+                <Pointer classname="text-[#2797FF] w-4" />
               </button>
             </div>
           </div>
         </div>
 
-        <div className="place-self-center text-center my-20 relative mb-40">
+        <div className="place-self-center text-center my-20 relative mb-40 px-6">
           <div className="flex flex-col items-center gap-20 z-10">
             <div className="flex flex-col">
               <h1>OUR</h1>
@@ -224,17 +229,17 @@ function LandingPage() {
               width={2000}
               height={2000}
               src={testimonials}
-              className="w-full p-10"
+              className="w-full md:p-10"
               alt="the Iphone Mockup"
             />
             <button className="buttonSemi z-10">
               GET US
-              <img width={15} height={15} src={pointer} alt="pointer" />
+              <Pointer classname="buttonIcon" />
             </button>
           </div>
         </div>
 
-        <div className="w-4/5 place-self-center text-center my-20 relative">
+        <div className="md:w-4/5 place-self-center text-center md:my-20 relative px-6 flex flex-col items-center gap-20">
           <div className="flex flex-col items-center gap-6 z-10">
             <MilkyanoCrown classname="w-10 text-[#1374F6]" />
             <div className="flex flex-col">
@@ -250,7 +255,7 @@ function LandingPage() {
             />
             <button className="buttonSemi z-10">
               GET US
-              <img width={15} height={15} src={pointer} alt="pointer" />
+              <Pointer classname="buttonIcon" />
             </button>
           </div>
 
@@ -258,7 +263,7 @@ function LandingPage() {
             width={200}
             height={200}
             src={robotHead}
-            className="absolute right-0 -bottom-20"
+            className="md:absolute md:right-0 md:-bottom-20"
             alt="the Iphone Mockup"
           />
         </div>

5c5cf32f69f2b4838c655c13202c5196d895d904
Darwin Prayoga <darwinprayoga@Darwins-MacBook-Air.local>
2024-07-23 14:07:34 +0700

    revision

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -115,7 +115,7 @@ function LandingPage() {
             width={1000}
             height={1000}
             src={astronout3d}
-            className="md:absolute left-52 -bottom-32 -z-10 order-last"
+            className="md:absolute left-52 -bottom-32 -z-10 order-last ml-3"
             alt="the Astronout 3d"
           />
 
@@ -135,18 +135,27 @@ function LandingPage() {
           </div>
         </div>
 
-        <div className="md:px-[438px] place-self-center my-20 flex flex-col md:flex-row justify-between items-center mb-20 md:mb-60 px-6 gap-20 md:gap-0">
+        <div className="md:px-[438px] place-self-center my-20 flex flex-col md:flex-row items-center mb-20 md:mb-60 px-6 gap-20 md:gap-0">
           <div className="px-16 md:pr-0 order-last relative">
-            <img
-              width={500}
-              height={500}
-              src={robot}
-              className="-z-10 w-full"
-              alt="the Iphone Mockup"
-            />
+            <div className="border border-[#007BFF] rounded-xl flex flex-col">
+              <img
+                width={500}
+                height={500}
+                src={robot}
+                className="-z-10"
+                alt="the Iphone Mockup"
+              />
+              <div className="flex justify-center items-center gap-6 my-10">
+                <p className="pointer">Yes, Please</p>
+                <button className="buttonNegative">
+                  GET ME THERE
+                  <Pointer classname="buttonIcon" />
+                </button>
+              </div>
+            </div>
           </div>
 
-          <div className="flex flex-col items-start gap-3 z-10 md:w-1/2">
+          <div className="flex flex-col items-start gap-3 z-10 md:w-4/5">
             <MilkyanoCrown classname="w-7 md:w-10 text-[#1374F6]" />
             <div className="flex flex-col">
               <h1>
@@ -178,8 +187,49 @@ function LandingPage() {
           </div>
         </div>
 
-        <div className="flex justify-center relative bg-gradient-to-b from-black to-[#01126B]/35 px-6">
-          <div className="md:px-[438px] flex flex-col items-start gap-3 z-10">
+        <div className="flex flex-col justify-center relative bg-gradient-to-b from-black to-[#01126B]/80 px-6 gap-20 md:gap-60">
+          <div className="md:px-[438px] flex flex-col md:flex-row justify-between relative">
+            <div className="transform scale-x-[-1] order-last md:order-first md:scale-x-100 w-56 md:w-80 place-self-end md:place-self-center">
+              <img
+                width={350}
+                height={350}
+                src={milkyanoRobot}
+                className="ml-8 md:ml-0"
+                alt="the Astronout 3d"
+              />
+            </div>
+
+            <div className="md:w-1/2 flex flex-col gap-3 z-10">
+              <MilkyanoCrown classname="w-7 md:w-10 text-[#1374F6]" />
+              <h1>OUR SERVICES</h1>
+              <sub className="mt-3">
+                Our primary goal is to maximize your ROI by driving conversion
+                sales through expertly crafted Facebook ads. We prioritize clear
+                and consistent communication, keeping you informed about our
+                strategies and progress. You'll have direct insight into our
+                actions and plans for your brand and products.
+              </sub>
+              <sub>
+                Flexibility is key in our approach. We adapt quickly to
+                incorporate your ideas, ensuring your advertising campaigns stay
+                innovative and effective.
+              </sub>
+              <sub>
+                With research at the forefront of our planning, we thoroughly
+                analyze your brand, products, and target audiences. This
+                meticulous approach builds a solid foundation to ensure your
+                brand not only stands out but thrives in the Facebook Ads
+                market. Our expertise guarantees increased sales, higher ROI,
+                and improved ROAS.
+              </sub>
+              <button className="buttonNegative w-max">
+                GET US
+                <Pointer classname="w-4" />
+              </button>
+            </div>
+          </div>
+
+          <div className="md:px-[438px] flex flex-col items-start gap-3 z-10 mb-32 md:mb-96">
             <MilkyanoCrown classname="w-7 md:w-10 text-[#1374F6]" />
             <div className="flex flex-col">
               <h1>
@@ -210,61 +260,17 @@ function LandingPage() {
                 their next creations.
               </sub>
             </div>
-            <button className="buttonSemi z-10">
+            <button className="buttonNegative z-10">
               GET US
               <Pointer classname="buttonIcon" />
             </button>
-
             <img
-              width={800}
-              height={800}
+              width={1000}
+              height={1000}
               src={threeAstronout3d}
-              className="self-center"
-              alt="the Iphone Mockup"
+              className="absolute bottom-0 left-0 right-0 place-self-center md:w-4/5"
+              alt="three astronout 3d milkyano"
             />
-          </div>
-        </div>
-
-        <div className="bg-[#2797FF] flex justify-center py-32 mb-10 px-6 h-screen md:h-max">
-          <div className="md:px-[438px] flex flex-col md:flex-row justify-between relative">
-            <div className="transform scale-x-[-1] order-last md:scale-x-100 w-56 md:w-60 place-self-end md:place-self-center">
-              <img
-                width={350}
-                height={350}
-                src={milkyanoRobot}
-                className="ml-8"
-                alt="the Astronout 3d"
-              />
-            </div>
-
-            <div className="md:w-1/2 flex flex-col gap-3 z-10">
-              <MilkyanoCrown classname="w-7 md:w-10" />
-              <h1>OUR SERVICES</h1>
-              <sub className="mt-3">
-                Our primary goal is to maximize your ROI by driving conversion
-                sales through expertly crafted Facebook ads. We prioritize clear
-                and consistent communication, keeping you informed about our
-                strategies and progress. You'll have direct insight into our
-                actions and plans for your brand and products.
-              </sub>
-              <sub>
-                Flexibility is key in our approach. We adapt quickly to
-                incorporate your ideas, ensuring your advertising campaigns stay
-                innovative and effective.
-              </sub>
-              <sub>
-                With research at the forefront of our planning, we thoroughly
-                analyze your brand, products, and target audiences. This
-                meticulous approach builds a solid foundation to ensure your
-                brand not only stands out but thrives in the Facebook Ads
-                market. Our expertise guarantees increased sales, higher ROI,
-                and improved ROAS.
-              </sub>
-              <button className="buttonNegative w-max">
-                GET US
-                <Pointer classname="text-[#2797FF] w-4" />
-              </button>
-            </div>
           </div>
         </div>
 
@@ -304,7 +310,7 @@ function LandingPage() {
                 alt="the testimonial"
               />
             </div>
-            <button className="buttonSemi z-10">
+            <button className="buttonOn z-10">
               GET US
               <Pointer classname="buttonIcon" />
             </button>
@@ -338,7 +344,7 @@ function LandingPage() {
                 alt="the carousel"
               />
             </div>
-            <button className="buttonSemi z-10">
+            <button className="buttonOn z-10">
               GET US
               <Pointer classname="buttonIcon" />
             </button>
